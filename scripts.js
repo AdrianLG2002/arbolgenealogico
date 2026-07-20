e9575a48f942b3ee62dac4a6b320d6ce6c7f1ae2
AdrianLG2002 <adrianisraellg2002@gmail.com>
2026-07-19 21:39:51 -0500

    Eliminando los divs y agregando funcionalidad al formulario

--- scripts.js
+++ scripts.js
@@ -12,3 +12,33 @@ function cerrarModal() {
 
 btnAbrir.addEventListener("click", abrirModal);
 btnCancelar.addEventListener("click", cerrarModal);
+
+const form = document.querySelector(".cuestionario form");
+const inputNombre = document.querySelector('input[type="text"]');
+const inputFecha = document.querySelector('input[type="date"]');
+const inputFoto = document.querySelector('input[type="file"]');
+
+let personas = [];
+let contadorId = 1;
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nombre = inputNombre.value;
+  const fecha = inputFecha.value;
+  const archivoFoto = inputFoto.files[0];
+
+  const nuevaPersona = {
+    id: contadorId,
+    nombre: nombre,
+    nacimiento: fecha,
+    foto: null,
+  };
+
+  contadorId = contadorId + 1;
+  personas.push(nuevaPersona);
+  console.log(personas);
+
+  form.reset();
+  cerrarModal();
+});
